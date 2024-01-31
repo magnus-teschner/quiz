70d5480490137d871a43d5ce37190277ec5e2a71
Magnus Teschner <magnus.2002.mt@gmail.com>
2024-01-31 11:04:04 +0100

    Add own quiz and middleware

---
+++ src2/client/Player.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+
+@dataclass(order=True)
+class Player():
+    points:int
+    uuid:str
+    name:str
+
+class PlayersList():
+    def __init__(self):
+        self.playerList = {} # {uuid: Player}
+    
+    def printLobby(self):
+        print("GAME LOBBY".center(40,'_'))
+        for player in sorted(self.playerList.values(), reverse=True):
+            print('{:<30}'.format(player.name), " | ", player.points)
+        
+    
+    def addPlayer(self, uuid:str, name:str, points:int = 0):
+        self.playerList[uuid] = Player(points, uuid,name)
+    
+    def toString(self): # , , #
+        s = ''
+        for uuid, player in self.playerList.items():
+            s += str(uuid)+','+str(player.name)+','+str(player.points)+'#'
+        return s
+    
+    def addPoints(self, uuid:str, points:int):
+        self.playerList[uuid].points += points
+
+    def updateList(self, playersList:str):
+        assert playersList[-1] =='#', f"the last character should be a #, maybe the string: {playersList} is empty"
+        players = playersList.split('#')[0:-1]
+        assert len(players) >= 2, "in this list there should be at least the sender and me"
+        for player in players:
+            player = player.split(',')
+            uuid = player[0]
+            name = player[1]
+            points = int(player[2])
+            self.addPlayer(uuid, name, points)
+        self.printLobby()
---
+++ src2/client/Quiz.py
@@ -0,0 +1,61 @@
+import sys
+import os
+import uuid
+from time import sleep
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
+from middleware.middleware import Middleware
+import time
+from Player import PlayersList
+
+import msvcrt
+
+states = {}
+class Statemachine():
+    UUID = str(uuid.uuid4())
+    players = PlayersList()
+    playerName = ''
+    gameRoomPort = 61424
+    currentState = ''
+    question = ''
+    answers = []
+
+    class State:
+        total_States = 0
+        def __init__(self, name):
+            self.name = name
+            self.id = self.total_States
+            self.total_States += 1
+            states[name] = self
+
+        def run(self):
+            pass
+
+    @classmethod
+    def switchToState(cls, targetState):
+        if "exit" in dir(states[cls.currentState]):
+            states[cls.currentState].exit()
+
+        if "entry" in dir(states[cls.currentState]):
+            states[cls.currentState].entry()
+
+        cls.currentState = targetState
+
+    def __init__(self):
+        self.middleware = Middleware(Statemachine.UUID, self)
+        Statemachine.currentState = "Initializing"
+        tempState = self.State("Initializing")
+        def state_initializing():
+            print(f"UUID: {Middleware.MY_UUID}")
+            self.playerName = input("Select your Player Name: ")
+            gamePort = input("Select Game Room Port: \nLeave Empty for default value of 60000")
+            self.gameRoomPort = (int(gamePort) if gamePort else 60000)
+
+a = Statemachine()
+
+
+
+
+
+
+
+
