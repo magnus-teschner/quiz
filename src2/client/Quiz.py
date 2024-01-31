bce9f8b5c2fed2c3d23f207421865b2c78f7638e
Magnus Teschner <magnus.2002.mt@gmail.com>
2024-01-31 11:43:04 +0100

    Send broadcast message

--- src2/client/Quiz.py
+++ src2/client/Quiz.py
@@ -35,8 +35,8 @@ class Statemachine():
         if "exit" in dir(states[cls.currentState]):
             states[cls.currentState].exit()
 
-        if "entry" in dir(states[cls.currentState]):
-            states[cls.currentState].entry()
+        if "entry" in dir(states[targetState]):
+            states[targetState].entry()
 
         cls.currentState = targetState
 
@@ -47,17 +47,47 @@ class Statemachine():
         def state_initializing():
             print(f"UUID: {Middleware.MY_UUID}")
             self.playerName = input("Select your Player Name: ")
-            gamePort = input("Select Game Room Port: \nLeave Empty for default value of 60000")
+            gamePort = input("Select Game Room Port (Leave Empty for default value of 60000) : ")
             self.gameRoomPort = (int(gamePort) if gamePort else 60000)
+            self.players.addPlayer(Middleware.MY_UUID, self.playerName)
 
+            if True:
+                self.switchToState("Lobby")
         tempState.run = state_initializing
+        tempState = self.State("Lobby")
+
+        def Lobby_entry():
+            self.middleware.leaderUUID = Middleware.MY_UUID
+            #self.middleware.subscribeBroadcastListener(self.respondWithPlayerList)
+            #self.middleware.subscribeTCPUnicastListener(self.listenForPlayersList)
+
+            command = "enterLobby"
+            data = self.playerName
+            self.middleware.broadcastToAll(command, data)
+            print("send broadcast")
+        tempState.entry = Lobby_entry
+
+
+    def listenForPlayersList(self, messengerUUID:str, messengerSocket, command:str, playersList:str):
+        if command == 'PlayerList':
+            self.middleware.leaderUUID = messengerUUID
+            self.players.updateList(playersList)
+
+    def respondWithPlayerList(self, messengerUUID:str, command:str, playerName:str):
+        if command == 'enterLobby':
+            self.players.addPlayer(messengerUUID, playerName)
+            if Middleware.MY_UUID == self.middleware.leaderUUID:
+                self.middleware.sendIPAdressesto(messengerUUID)
+                responseCommand = 'PlayerList'
+                responseData = self.players.toString()
+                self.middleware.sendTcpMessageTo(messengerUUID, responseCommand, responseData)
 
     def runLoop(self):
         states[self.currentState].run()
 
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     print("Peer to peer quiz game")
     SM = Statemachine()
     while True:
